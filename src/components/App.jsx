dfaf45092f2fa5b584fd2f2884b46de0f3d4290a
Yuraro <romanykyriy@gmail.com>
2023-05-22 16:23:40 -0700

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { Profile } from "../components/Profile.jsx";
+import { Statistics } from "../components/Statistics.jsx";
+import data from '../data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
+
