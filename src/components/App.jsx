6acb8ec19261bb5d7c12bc773166865607affba5
Yuraro <romanykyriy@gmail.com>
2023-05-25 11:31:20 -0700

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { Profile } from "./Profile/Profile.jsx";
 import { Statistics } from "./Statistics/Statistics.jsx";
-import data from './configs/data.json';
-import friends from './configs/friends.json';
+import data from '../configs/data.json';
+import friends from '../configs/friends.json';
 import { FriendList } from "./FriendList/FriendList";
-import transactions from './configs/transactions.json';
+import transactions from '../configs/transactions.json';
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import css from './App.module.css';
 
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,21 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
-
-const FriendListItem = ({ avatar, name, isOnline }) => {
-    return (
-    <li className={css.item}>
-        <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
-    </li>
-);
-};
-
-FriendListItem.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-};
+import { FriendListItem } from "../FriendListItem/FriendListItem";
 
 export const FriendList = ({ friends }) => {
     return (
